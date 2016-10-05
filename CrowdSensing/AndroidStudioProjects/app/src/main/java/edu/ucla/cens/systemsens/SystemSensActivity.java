0fd843f81f2b9a0951b9447c930cd8b0e13c070d
LLADzhang <henry.zhang.king@gmail.com>
2016-10-05 13:14:13 -0400

    sync files

---
+++ CrowdSensing/AndroidStudioProjects/app/src/main/java/edu/ucla/cens/systemsens/SystemSensActivity.java
@@ -0,0 +1,15 @@
+package edu.ucla.cens.systemsens;
+
+import android.app.Activity;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+
+public class SystemSensActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_system_sens);
+    }
+}
---
+++ CrowdSensing/README.txt
@@ -0,0 +1,6 @@
+### Folder Structure ###
+
+AndroidStudioProjects - SystemSens in Android studio
+CODES - similar application
+SensorController - blank AndroidStudio project
+SystemSens-master - original SystemSens source files
