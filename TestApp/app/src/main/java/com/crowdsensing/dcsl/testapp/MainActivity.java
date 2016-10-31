37b86b83ca81437fc8536d31bc3b1da5c34bb039
raokrutarth <krk91@outlook.com>
2016-10-31 11:22:28 -0400

    TestApp sync button added

--- TestApp/app/src/main/java/com/crowdsensing/dcsl/testapp/MainActivity.java
+++ TestApp/app/src/main/java/com/crowdsensing/dcsl/testapp/MainActivity.java
@@ -2,12 +2,18 @@ package com.crowdsensing.dcsl.testapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void syncRClicked(View view) {
+        // TODO for sync button clicked
     }
 }
