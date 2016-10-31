7e819dfc8f6eba2cf37b148a83fbeff324f16d39
raokrutarth <krk91@outlook.com>
2016-10-31 12:01:34 -0400

    TestApp status added

--- TestApp/app/src/main/java/com/crowdsensing/dcsl/testapp/MainActivity.java
+++ TestApp/app/src/main/java/com/crowdsensing/dcsl/testapp/MainActivity.java
@@ -3,6 +3,7 @@ package com.crowdsensing.dcsl.testapp;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,9 +12,14 @@ public class MainActivity extends AppCompatActivity {
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+
     }
 
     public void syncRClicked(View view) {
+        TextView statusMsg = (TextView) findViewById(R.id.statusMessage);
+        statusMsg.setText("Sync functionality not yet implemented");
         // TODO for sync button clicked
     }
 }
