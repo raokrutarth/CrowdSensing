37f21a1901ae7dbfabb2ffb1736117cefd924cbb
raokrutarth <krk91@outlook.com>
2016-11-03 11:38:33 -0400

    funf framework intergrted into app

--- TestApp/app/src/main/java/com/crowdsensing/dcsl/testapp/MainActivity.java
+++ TestApp/app/src/main/java/com/crowdsensing/dcsl/testapp/MainActivity.java
@@ -1,25 +1,34 @@
 package com.crowdsensing.dcsl.testapp;
 
+import android.app.AlarmManager;
+import android.app.PendingIntent;
+import android.content.Context;
+import android.content.Intent;
+import android.hardware.Sensor;
+import android.hardware.SensorManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity
+{
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-
-
     }
 
     public void syncRClicked(View view) {
+
+        SensorManager sensorManager = (SensorManager) getSystemService(SENSOR_SERVICE);
+        Sensor sensor = sensorManager.getDefaultSensor(Sensor.TYPE_PRESSURE);
+
         TextView statusMsg = (TextView) findViewById(R.id.statusMessage);
         statusMsg.setText("Sync functionality not yet implemented");
+
         // TODO for sync button clicked
     }
 }
